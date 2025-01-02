14be0d0e2ec3b5f21ea696615a714790bf0d1c50
shivamkoli <67708684+shivam7053@users.noreply.github.com>
2025-01-02 16:39:52 +0000

    second

--- social_app/settings.py
+++ social_app/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = "social_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_db',
-        'USER': 'django_user',
-        'PASSWORD': 'django_password',
-        'HOST': 'db',  # matches service name in docker-compose.yml
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'django_db',
+#         'USER': 'django_user',
+#         'PASSWORD': 'django_password',
+#         'HOST': 'db',  # matches service name in docker-compose.yml
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
